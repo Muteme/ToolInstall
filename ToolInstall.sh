d17c57c0c18205bc77bb1fb10f526e3ed1508c9d
Muteme <chaidclark@gmail.com>
2021-02-01 22:12:36 -0800

    Update ToolInstall.sh

--- ToolInstall.sh
+++ ToolInstall.sh
@@ -1,3 +1,3 @@
 !/bin/bash
 
-git clone https://github.com/epi052/feroxbuster.git && git clone https://github.com/danielmiessler/SecLists.git
+git clone https://github.com/epi052/feroxbuster.git && git clone https://github.com/danielmiessler/SecLists.git && git clone https://github.com/maurosoria/dirsearch.git
